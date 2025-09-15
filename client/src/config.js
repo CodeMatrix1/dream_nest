dbd9da6ed35c957eece873dd904d46f15aaf5e3d
CodeMatrix1 <manyamsrivallabh@gmail.com>
2025-09-15 15:53:29 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@
 // For Create React App, env vars must be prefixed with REACT_APP_
 
 export const API_BASE_URL =
-  process.env.serve_port || "http://localhost:3001";
+  process.env.serve_port || "https://dream-nest-1-6cwg.onrender.com";
 
 
